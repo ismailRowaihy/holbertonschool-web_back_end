27a308d73bb7bee26f4e6cd5d73a2c7eb8f04dc9
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-05 07:13:38 +0000

    a function that takes a float and retruns it as string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module is for a function that takes a float and strings it
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    this fucntion takes a float retruns it as a string
+    """
+    return str(n)
