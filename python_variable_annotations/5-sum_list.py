ec61a3298d34c5efb237db62bd5aa7d58ec10638
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-05 07:30:46 +0000

    a function that retruns the sum of a float list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module is a function that takes a list of float and returns a float
+"""
+
+
+def sum_list(input_list : list[float]) -> float:
+    """
+    this fucntion takes a list of float retruns its sum as a float
+    """
+    return sum(input_list)
