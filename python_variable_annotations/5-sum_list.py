ac581d383b6bff760b1b3878d51a5235fad93490
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-05 07:42:46 +0000

    fixed doc

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import typing
-
 """
 the module is a function that takes a list of float and returns a float
 """
+import typing
 
 
 def sum_list(input_list: typing.List[float]) -> float:
