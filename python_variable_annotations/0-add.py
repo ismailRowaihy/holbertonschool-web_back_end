ae8ed9978befde0de63c2c234943d76885dca11f
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-05 06:57:05 +0000

    fixed pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,8 +3,9 @@
 the module is for a function that adds 2 float num
 """
 
-def add(a:float,b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """
     this fucntion takes 2 floats and adds them and retruns a float
     """
-    return a+b
+    return a + b
