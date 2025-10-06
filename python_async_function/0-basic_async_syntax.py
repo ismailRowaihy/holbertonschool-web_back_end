aebd84b72cfe50a0f30a898ba54363f527cc5da7
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-06 07:52:18 +0000

    a function that sleeps with a random value

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+"""this module with afunction that sleeps with a random value """
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """a function that sleeps with random value"""
+    R = random.uniform(0, max_delay)
+    await asyncio.sleep(R)
+    return R
