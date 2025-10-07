2bb9dc78030f99e0b5eb0506b403a929c09b7fba
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-07 07:30:57 +0000

    made it concurrent with gather

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,10 +10,8 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """a function that sleeps with random value"""
-    list_Delays = []
-    asyncio.get_running_loop()
-    for i in range(n):
-        num = await wait_random(max_delay)
-        list_Delays.append(num)
+
+    tasks = [wait_random(max_delay) for i in range(n)]
+    list_Delays = await asyncio.gather(*tasks)
 
     return sorted(list_Delays)
