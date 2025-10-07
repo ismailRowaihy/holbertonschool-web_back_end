457b7cd6f1e7304511ba3993fcb41f04c49737d5
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-07 07:53:12 +0000

    fixed pycodestyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,10 +4,11 @@
 
 import asyncio
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
-    """a function measures sleeps from """
+    """a function measures sleeps from"""
     total_time = asyncio.run(wait_n(n, max_delay))
 
     return sum(total_time) / n
