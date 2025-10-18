488394a71b238dc53608f7aec3f114e70e5d586b
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-18 10:23:48 +0000

    a function that uses async loop to loop over async generator

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -11,8 +11,4 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> typing.List[float]:
     """a function that uses asyncloops over async generator"""
-
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
