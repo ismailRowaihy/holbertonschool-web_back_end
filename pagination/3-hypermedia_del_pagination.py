dd0fc3cb9db5167a6cece9d783d071aacc80ffde
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-21 06:24:24 +0000

    fixed pycodestyle

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -9,8 +9,8 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,33 +28,39 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
-            }
+                }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            """a method that returns data as a dict"""
-            assert isinstance(index, int) and 0 <= index < len(self.__indexed_dataset)
-            
-            keys = sorted(self.__indexed_dataset.keys())
+        """a method that returns data as a dict"""
+        assert isinstance(index, int)
+        assert 0 <= index < len(self.__indexed_dataset)
 
-            start_range = 0
-            while start_range < len(keys)and keys[start_range]< index:
-                start_range +=1
+        keys = sorted(self.__indexed_dataset.keys())
 
-            valid_keys = keys[start_range:start_range + page_size]
+        start_range = 0
+        while start_range < len(keys) and keys[start_range] < index:
+            start_range += 1
 
-            data_list=[self.__indexed_dataset[k] for k in valid_keys]
-            next_index = keys[start_range + page_size] if start_range + page_size < len(keys) else None
-            
-            data_dict={"index":index,
-                      "data":data_list,
-                      "page_size":page_size,
-                      "next_index":next_index}
-            return data_dict
+        valid_keys = keys[start_range: start_range + page_size]
+
+        data_list = [self.__indexed_dataset[k] for k in valid_keys]
+        next_index = (
+            keys[start_range + page_size]
+            if start_range + page_size < len(keys)
+            else None
+        )
+
+        data_dict = {
+            "index": index,
+            "data": data_list,
+            "page_size": page_size,
+            "next_index": next_index,
+        }
+        return data_dict
