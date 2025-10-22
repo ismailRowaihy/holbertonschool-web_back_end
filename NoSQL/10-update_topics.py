baeb5ec55cae9f1c838e3d7009a0b9db740af7fc
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-22 10:40:59 +0300

    added a fucntion that updates data in a doc in col

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this module is a fucntin that updates data to a collection"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """a function that updates data to a collection"""
+    mongo_collection.update_one({"name":name},
+    {"$set": {"topics":topics}})
+
