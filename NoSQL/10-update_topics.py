cf87ba126d660d0e2368e635d3372d3916ae6528
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-22 10:47:29 +0300

    changed from update one to update many

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,4 +5,4 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """a function that updates data to a collection"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
