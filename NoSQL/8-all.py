f2f3f5f91d2831a023b0395f1761532c7a0f7288
ismailRowaihy <ismailRowaihy@gmail.com>
2025-10-22 10:10:07 +0300

    fixed pycodestyle and doc

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,10 @@
-""""""
+"""this module is a fucntin that returns a list of docs """
 import pymongo
 
 
 def list_all(mongo_collection):
+    """a function that returns a list of the docs"""
     data = list(mongo_collection.find())
     if len(data) == 0:
         return []
     return data
-
