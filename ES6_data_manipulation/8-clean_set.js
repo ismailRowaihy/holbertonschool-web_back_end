9f61cd2b024ece5e4f9a7a91dc573c697231db45
ismailRowaihy <ismailRowaihy@gmail.com>
2025-11-04 04:27:44 +0000

    added check if no SS is passed

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet (set,startString){
-    if (startString && startString.length < 1){
+    if (typeof startString !== 'string' || startString.length < 1){
         return ''
     }
     return Array.from(set)
