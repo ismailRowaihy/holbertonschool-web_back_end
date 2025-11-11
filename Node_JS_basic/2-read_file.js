ebec69177ba1eebc59d8ae8925eed26a8cf490dc
ismailRowaihy <ismailRowaihy@gmail.com>
2025-11-11 08:45:48 +0000

    a function that reads a csv file and prints num of students and what field they in

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,33 @@
-const fs = require('node:fs')
+const fs = require('node:fs');
 
-function countStudents (path){
-    fs.readFile(path,'utf8',(err,data) => {
-        if (err){
-            throw new Error("Cannot load the database");
-        }
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
 
-        lines = data.trim().split('\n');
-        const headers = lines[0].split(',');
-        lines.shift()
+    const lines = data.trim().split('\n');
+    // const headers = lines[0].split(',');
+    lines.shift();
 
-        const fieldMap = new Map()
+    const fieldMap = new Map();
 
-        for (let line of lines){
-            i = line.split(',')
-            if (fieldMap.has(i[3])){
-                field = fieldMap.get(i[3])
-                field.push(i[0])
-            }else{
-                fieldMap.set(i[3],[i[0]])
-            }
-        }
-        //console.log(fieldMap)
-        console.log(`Number of students: ${lines.length}`)
-        
-        fieldMap.forEach((value,key) =>{
-            console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`)
-        })
-    })
+    for (const line of lines) {
+      const i = line.split(',');
+      if (fieldMap.has(i[3])) {
+        const field = fieldMap.get(i[3]);
+        field.push(i[0]);
+      } else {
+        fieldMap.set(i[3], [i[0]]);
+      }
+    }
+    // console.log(fieldMap)
+    console.log(`Number of students: ${lines.length}`);
+
+    fieldMap.forEach((value, key) => {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    });
+  });
 }
 
 module.exports = countStudents;
