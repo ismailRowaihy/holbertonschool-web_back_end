a5ac316b40e2870bfaa3ce43527cacea49c56b59
ismailRowaihy <ismailRowaihy@gmail.com>
2025-11-12 06:49:19 +0000

    simple node server

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 1245;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
