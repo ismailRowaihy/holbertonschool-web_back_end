05ee00b6345ffa4d4539024a60dd89b7318c8a6a
ismailRowaihy <ismailRowaihy@gmail.com>
2025-11-12 08:07:45 +0000

    http server with a page that useis a function to print some shit

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+
+
+
+const hostname = '0.0.0.0';
+const port = 1245;
+
+
+const app = http.createServer(async(req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello Holberton School!');
+  }else if (req.url === '/students' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write( await countStudents(process.argv[2]))
+    res.end()
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
